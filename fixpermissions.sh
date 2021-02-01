e584d95de33b53aa500da69c8d70759e3d59083c
James A. Chambers <05jchambers@gmail.com>
2021-02-01 02:00:53 -0700

    Added fixpermissions.sh utility script to take ownership of folder files (service does this automatically now)

--- fixpermissions.sh
+++ fixpermissions.sh
@@ -4,7 +4,7 @@
 # Takes ownership of server files to fix common permission errors such as access denied
 # This is very common when restoring backups, moving and editing files, etc.
 
-# If you are using the systemd service (sudo systemctl start minecraft) it performs this automatically for you each startup
+# If you are using the systemd service (sudo systemctl start servername) it performs this automatically for you each startup
 
 echo "Taking ownership of all server files/folders in dirname/minecraftbe/servername"
 sudo chown -Rv userxname dirname/minecraftbe/servername
