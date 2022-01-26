271ce276fbca69f6b85fdfb81fe9b39e4549f985
KurehaIkeda <kuuchanxn@gmail.com>
2022-01-26 23:34:23 +0900

    modified:   serverfiles/stop_and_backup_for_restart.sh

--- serverfiles/stop_and_backup_for_restart.sh
+++ serverfiles/stop_and_backup_for_restart.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 if sudo screen -list | grep -q "\.servername"; then
     sudo screen -Rd servername -X stuff "say バックアップのため、25秒後にサーバーを閉じます。$(printf '\r')"
-    sleep 18s
-    sudo screen -Rd servername -X stuff "say サーバーを7秒後に閉じます$(printf '\r')"
-    sleep 1s
-    sudo screen -Rd servername -X stuff "say サーバーを6秒後に閉じます$(printf '\r')"
-    sleep 1s
-    sudo screen -Rd servername -X stuff "say サーバーを5秒後に閉じます$(printf '\r')"
-    sleep 1s
-    sudo screen -Rd servername -X stuff "say サーバーを4秒後に閉じます$(printf '\r')"
-    sleep 1s
+    sleep 22s
     sudo screen -Rd servername -X stuff "say サーバーを3秒後に閉じます$(printf '\r')"
     sleep 1s
     sudo screen -Rd servername -X stuff "say サーバーを2秒後に閉じます$(printf '\r')"
