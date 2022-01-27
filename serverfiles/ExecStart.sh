3f12d7556a9f155a1eb35406b93fa46ae23ff10b
KurehaIkeda <kuuchanxn@gmail.com>
2022-01-27 18:06:22 +0900

    modified:   ExecStart.sh

--- serverfiles/ExecStart.sh
+++ serverfiles/ExecStart.sh
@@ -3,5 +3,6 @@ if sudo screen -ls | grep -q "\.servername"; then
     exit 0
 fi
 sudo screen -L -Logfile /home/username/servername/$(date "+%Y%m%d%H%M").log -dmS servername /bin/bash -c 'serverstartcommand'
+sleep 1s
 sudo screen -Rd servername -X stuff "say サーバーを開始します$(printf '\r')"
 python3 -u /home/username/servername/minecord.py
