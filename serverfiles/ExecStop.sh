f1f1095ca2d99660e3a5c0a63f8a8dc079051ae7
KurehaIkeda <kuuchanxn@gmail.com>
2022-01-27 16:29:56 +0900

    modified:   serverfiles/ExecStop.sh

--- serverfiles/ExecStop.sh
+++ serverfiles/ExecStop.sh
@@ -3,7 +3,7 @@ if ! sudo screen -list | grep -q "\.servername"; then
     exit 0
 fi
 sudo screen -Rd servername -X stuff "say サーバーを閉じます...$(printf '\r')"
-sleep 10s
+sleep 5s
 sudo screen -Rd servername -X stuff "stop$(printf '\r')"
 StopChecks=0
 while [[ $StopChecks -lt 10 ]]; do
