ba86cbb15a261edbbfee081ea24e9133d90b201c
KurehaIkeda <kuuchanxn@gmail.com>
2022-01-27 09:04:06 +0900

    modified

--- serverfiles/ExecStart.sh
+++ serverfiles/ExecStart.sh
@@ -4,3 +4,5 @@ if sudo screen -list | grep -q "\.servername"; then
 fi
 sudo screen -L -Logfile /home/username/servername/$(date "+%Y%m%d%H%M").log -dmS servername /bin/bash -c 'serverstartcommand'
 python3 /home/username/servername/minecord.py &
+sleep 10s
+sudo screen -Rd servername -X stuff "say サーバーを開始します$(printf '\r')"
