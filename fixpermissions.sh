787f542ae6a12d4ee2bae7477f3385bcdbe6c80f
Nicholas Buse <NicholasBuse@users.noreply.github.com>
2022-04-28 06:14:24 -0500

    shellcheck on fixpermissions.sh
    Two issues. Fixed the empty if statement, not sure intent on case statement (getopts asks for -a, case checks -t)

--- fixpermissions.sh
+++ fixpermissions.sh
@@ -50,6 +50,7 @@ else
     NewestLog=$(find dirname/minecraftbe/servername/logs -type f -exec stat -c "%y %n" {} + | sort -r | head -n1 | cut -d " " -f 4-)
     if [ -z "$NewestLog" ]; then
       # No logs found
+      true
     else
       echo "Displaying last 10 lines from log file $NewestLog in /logs folder:"
       tail -10 "$NewestLog"
