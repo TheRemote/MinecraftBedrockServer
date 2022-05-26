01cab430457c7486e2f0d221c5403cfab5938d7a
James Chambers <05jchambers@gmail.com>
2022-05-25 22:45:37 -0600

    Added revert.sh documentation

--- revert.sh
+++ revert.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Minecraft Server Permissions Fix Script - James A. Chambers - https://jamesachambers.com/minecraft-bedrock-edition-ubuntu-dedicated-server-guide/
 
+# Set version in version_pin.txt
 # Reverts version pinning if you set a custom version to avoid updates being out of sync with Microsoft's servers
-
-# If you are using the systemd service (sudo systemctl start servername) it performs this automatically for you each startup
+# Removing this file will enable automatic updates again
 
 ls -r1 downloads/ | grep bedrock-server | head -2 | tail -1 > version_pin.txt
 echo "Set previous version in version_pin.txt: $(cat version_pin.txt)"
