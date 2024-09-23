819cc236e2cd90f3535d93667fa3e2fdd93834f5
M-Taylor40 <44699720+M-Taylor40@users.noreply.github.com>
2024-09-23 20:44:57 +0100

    Update SetupMinecraft.sh
    Updating DownloadURL with updated download links.

--- SetupMinecraft.sh
+++ SetupMinecraft.sh
@@ -223,7 +223,7 @@ Update_Server() {
   # Retrieve latest version of Minecraft Bedrock dedicated server
   echo "Checking for the latest version of Minecraft Bedrock server..."
   curl -H "Accept-Encoding: identity" -H "Accept-Language: en" -L -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.33 (KHTML, like Gecko) Chrome/90.0.$RandNum.212 Safari/537.33" -o downloads/version.html https://minecraft.net/en-us/download/server/bedrock/
-  DownloadURL=$(grep -o 'https://minecraft.azureedge.net/bin-linux/[^"]*' downloads/version.html)
+  DownloadURL=$(grep -o 'https://www.minecraft.net/bedrockdedicatedserver/bin-linux/[^"]*' downloads/version.html)
   DownloadFile=$(echo "$DownloadURL" | sed 's#.*/##')
   echo "$DownloadURL"
   echo "$DownloadFile"
